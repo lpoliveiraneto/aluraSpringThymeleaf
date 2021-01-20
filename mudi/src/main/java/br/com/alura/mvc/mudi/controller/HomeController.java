ab30cd08081d0eeb9b9f6fdffc30b959b2608639
lpoliveiraneto <leonidespires@gmail.com>
2021-01-20 00:33:59 -0300

    paginação e habilitação de cache

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -4,6 +4,8 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,22 +27,12 @@ public class HomeController {
 	@GetMapping
 	public String home(Model model, Principal principal) {
 		
-		List<Pedido> pedidos = pedidoRepository.findAll(); 
-		model.addAttribute("pedido", pedidos);
-		return "home";
-	}
-	
-	@GetMapping("/{status}")
-	public String porStatus(@PathVariable("status") String status, Model model) {
+		Sort sort = Sort.by("dataDaEntrega").descending();
+		PageRequest paginacao = PageRequest.of(0, 10, sort);
 		
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase())); 
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao); 
 		model.addAttribute("pedido", pedidos);
-		model.addAttribute("status", status);
 		return "home";
 	}
 	
-	@ExceptionHandler(IllegalArgumentException.class)
-	public String onError() {
-		return "redirect:/home";
-	}
 }
